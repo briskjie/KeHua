a86c2dedb855cc0061254efc27e1d16a6a8cca67
chenxiangxu <chenxiangxu@baidu.com>
2025-09-17 14:51:12 +0800

    first comit

---
+++ KeHua/KeHuaApp.swift
@@ -0,0 +1,32 @@
+//
+//  KeHuaApp.swift
+//  KeHua
+//
+//  Created by chenxiangxu on 2025/9/17.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KeHuaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
